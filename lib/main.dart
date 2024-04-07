f0b9caed347859cb65d59090cde8bcb0e043beb2
joel-tito <jtito.alexander@gmail.com>
2024-04-07 15:07:22 -0500

    se agrego le text hola

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Text("hola")
           ],
         ),
       ),
