2376551d44c0515b593481f6deb7a653352913d4
Joel Alexander Tito Quijije <jtito.alexander@gmail.com>
2024-04-26 17:10:14 -0500

    la logica con el red neuronal para recibir a paypal  terminado

--- lib/calculadoraPaypal.dart
+++ lib/calculadoraPaypal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class CalculadoraPaypal extends StatefulWidget {
   @override
@@ -6,11 +7,47 @@ class CalculadoraPaypal extends StatefulWidget {
 }
 
 class _CalculadoraPaypalState extends State<CalculadoraPaypal>{
+   // Declara un objeto Interpreter que se utilizará para cargar el modelo TensorFlow Lite.
+  Interpreter? interpreter;
+
+  // Variable para almacenar el valor de la predicción.
+  var predValue = "";
+
+  // Método initState, se llama cuando el widget se inserta en el árbol de widgets.
+  @override
+  void initState() {
+    super.initState();
+    // Inicializa el modelo TensorFlow Lite cuando se carga el widget.
+    initModel();
+  }
+
+  // Método para inicializar el modelo TensorFlow Lite.
+  initModel() async {
+    // Carga el modelo TensorFlow Lite desde un archivo asset.
+    final _interpreter = await Interpreter.fromAsset('modeloPaypal.tflite');
+    setState(() {
+      interpreter = _interpreter;
+    });
+  }
+
+  // Método para obtener la predicción utilizando el modelo TensorFlow Lite.
+  getPrediction(input){
+    // Crea un tensor para almacenar la salida del modelo.
+    var output = List.filled(1, 0).reshape([1, 1]);
+    // Ejecuta el modelo TensorFlow Lite con la entrada proporcionada y almacena la salida en el tensor de salida.
+    interpreter?.run(input, output);
+    // Obtiene el valor de predicción de la salida del modelo y lo formatea.
+    var prediccion = output[0][0].toStringAsFixed(1).toString();
+    return prediccion;
+  }
+  
   double amountToReceive = 0.0;
   double commissionPercentage = 5.4;
   double fixedCommission = 0.30;
   double totalCommission = 0.0;
   double amountToSend = 0.0;
+  double cobroRecibir = 0.0;
+  double gananciaRecibir = 0.0;
 
   double montoRecibido = 0.0;
   double comisionEnv = 13.0;
@@ -29,43 +66,42 @@ class _CalculadoraPaypalState extends State<CalculadoraPaypal>{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            //Text('Las Comisiones PayPal',
-              //style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            //),
-            //SizedBox(height: 10),
-            //Text('5,4% más \$0,30 USDs'),
-            //SizedBox(height: 20),
             Text(
-              'Calculadora PayPal para Recibir',
+              'Cliente Recibe a Paypal',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             TextField(
               decoration: InputDecoration(
-                labelText: 'Para Recibir (USDs)',
+                labelText: 'Para que el cliente Reciba (USDs) en Paypal',
               ),
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               onChanged: (value) {
                 setState(() {
                   amountToReceive = double.tryParse(value) ?? 0.0;
-                  calculateCommission();
+                  predValue = getPrediction([amountToReceive]);
+                  calculateValorEnviar();
                 });
               },
             ),
             SizedBox(height: 10),
-            Text('Hay que Enviar: \$${amountToSend.toStringAsFixed(2)} USDs'),
+            Text('Hay que Enviar: \$${predValue} USDs'),
             SizedBox(height: 10),
             Text('La Comisión es de: \$${totalCommission.toStringAsFixed(2)} USDs'),
-
+            SizedBox(height: 10),
+            Text('Se debe cobrar: \$${cobroRecibir.toStringAsFixed(2)} USDs'),
+            SizedBox(height: 10),
+            Text('La Ganacia es de: \$${gananciaRecibir.toStringAsFixed(2)} USDs'),
+            
             SizedBox(height: 40),
             Text(
-              'PayPal para enviar al Cliente',
+              'Cliente Envia a Paypal',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             TextField(
               decoration: InputDecoration(
-                labelText: 'Si el cliente Envia (USDs)',
+                labelText: 'Si el cliente Envia (USDs) a Paypal',
               ),
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               onChanged: (value) {
@@ -87,17 +123,18 @@ class _CalculadoraPaypalState extends State<CalculadoraPaypal>{
     );
   }
 
-  void calculateCommission() {
-    double percentageCommission = (amountToReceive * commissionPercentage) / 100;
-    totalCommission = percentageCommission + fixedCommission;
-    amountToSend = amountToReceive + totalCommission;
+   void calculateValorEnviar() {
+    double percentageCommission = double.parse(predValue) - amountToReceive;
+    totalCommission = percentageCommission;
+    cobroRecibir =  amountToReceive*1.20;
+    gananciaRecibir = cobroRecibir - double.parse(predValue);
   }
 
-    void calculateCommissionEnvio() {
-    double percentageCommission = (montoRecibido * comisionEnv) / 100;
-    double comisionPaypal = (montoRecibido * commissionPercentage) / 100;
-    ganancia = percentageCommission - comisionPaypal;
-    totalComision = percentageCommission + fixedCommission;
+  void calculateCommissionEnvio() {
+    double percentageCommission = (montoRecibido * 13.0) / 100;
+    double comisionPaypal = (montoRecibido * 5.0) / 100;
     montoEnviar = montoRecibido - totalComision;
+    totalComision = percentageCommission + 0.30;
+    ganancia = percentageCommission - comisionPaypal;
   }
 }
